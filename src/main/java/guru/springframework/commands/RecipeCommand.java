764547ef45c82a86f9f173c4866fc6419866e365
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-10-07 13:01:10 +0300

    Error delete9

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -1,11 +1,13 @@
 package guru.springframework.commands;
 
 import guru.springframework.domain.Difficulty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
+
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -21,6 +23,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private String id;
+
 
     @NotBlank
     @Size(min = 3, max = 255)
--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -3,6 +3,7 @@ package guru.springframework.controllers;
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+
 
 /**
  * Created by jt on 6/19/17.
